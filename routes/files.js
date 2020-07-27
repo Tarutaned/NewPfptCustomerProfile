3c07afe9aefec7f709d60d19453f152154ff9d83
Tarutaned <martin.engineer@rogers.com>
2020-07-27 15:31:57 -0400

    SE-372: Improved the File Upload and download functionality with AJAX.

--- routes/files.js
+++ routes/files.js
@@ -53,10 +53,12 @@ const storage = new GridFsStorage({
 
 
 
-// @route POST /upload
-// @desc Uploads file to DB
+// ======================================================
+// API Endpoint
+// Upload a file
+// ======================================================
 router.post('/upload', connectEnsureLogin.ensureLoggedIn(), upload.single('file'), (req, res) => {
-  console.log('[+] Post upload: ' + req.file.originalname)
+  console.log(getTimeStamp() + req.user.sAMAccountName + " uploading file: " + req.file.originalname)
   if (!(req.file === undefined)) {
     const cf = new customerfile( {
       fileID: res.req.file.id,  // FIX THIS !!!?!?!?!
@@ -74,21 +76,20 @@ router.post('/upload', connectEnsureLogin.ensureLoggedIn(), upload.single('file'
         return res.render('error.ejs', {message: "Duplicate File Exists", user: req.user})
       }
 
-      console.log("[-] Something went wrong!!!")
+      console.log(getTimeStamp() + req.user.sAMAccountName + " Something went wrong!!!")
       console.log(e)
       return res.render('error.ejs', {message: e, user: req.user})
     })
     //return res.render('upload.ejs', {success: "File uploaded successfully"})
   } else {
-    console.log('[+] No file was selected.')
+    console.log(getTimeStamp() + req.user.sAMAccountName + '[+] No file was selected.')
     return res.render('upload', {error: "Please select a file"})
   }
 })
 
 
-
 // ======================================================
-// GET
+// API Endpoint
 // Lists all files associated with a customerID
 // ======================================================
 router.post('/listfiles/:customername', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
@@ -98,33 +99,6 @@ router.post('/listfiles/:customername', connectEnsureLogin.ensureLoggedIn(), (re
     return res.render('error.ejs', {message: e, user: req.user})
   })
 })
-
-router.post('/echo', (req, res) => {
-  console.log(req.body)
-  const response = req.body.search
-  res.send(response)
-})
-
-// @route GET /files
-// @desc Display all files in JSON
-router.get('/files', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
-  console.log('[+] Get Files')
-  gfs.files.find().toArray( (err, files) => {
-    if(err) {
-      return res.status(500).json({ err })
-    }
-
-    // Check if files exist
-    if(!files || files.length === 0) {
-      return res.render('files.ejs')
-    }
-    else {
-      // Files exist
-      return res.render('files.ejs', {files, user: req.user})
-    }
-  })
-})
-
 
 
 // =============================================
@@ -143,54 +117,13 @@ router.get('/getFiles/:customername', connectEnsureLogin.ensureLoggedIn(), (req,
   })
 })
 
-// @route GET /files/:filename
-// @desc Display one file object in JSON
-router.get('/files/:filename', (req, res) => {
-  console.log('[+] Get file: ' + req.params.filename)
-  gfs.files.findOne({filename: req.params.filename }, (err, file) => {
-    if(err) { return res.status(500).json({ err }) }
-    if(!file || file.length === 0) {
-      return res.status(404).json({ err: 'file does not exist'})
-    }
-    else {
-      // the file exists
-      return res.json(file)
-    }
-   })
-})
 
-// @route /GET /image
-// @desc Display an image
-router.get('/image/:filename', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
-  console.log('[+] Get image: ' + req.params.filename)
-  gfs.files.findOne({filename: req.params.filename}, (err, file) => {
-    if(err) { return res.status(500).json({ err }) }
-    if(!file || file.length === 0) { 
-      return res.status(404).json({ err: 'File does not exist'})
-    }
-    else {
-      // the file exists
-      if(file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-        
-        // Read output to browser
-        const readstream = gfs.createReadStream(file);
-        readstream.pipe(res);
-        
-      }
-      else {
-        const readstream = gfs.createReadStream(file);
-        readstream.pipe(res);
-        // return res.status(404).json({ err: 'This file is not an image'})
-      }
-      
-    }
-  })
-})
-
-// @route /GET /file
-// @desc Download a File by ID
+// =============================================
+// API Endpoint
+// Download a file by _id
+// =============================================
 router.get('/getfile/:_id', (req, res) => {
-  console.log('[+] Get file id: ' + req.params._id)
+  console.log(getTimeStamp() + req.user.sAMAccountName + ' downloading file id: ' + req.params._id)
   gfs.files.findOne({_id: ObjectId(req.params._id)}, (err, file) => {
     if(err) {return res.status(500).json( {err})}
     if(!file || file.length === 0) {
@@ -202,8 +135,6 @@ router.get('/getfile/:_id', (req, res) => {
     else {
       // the file exists
       const readstream = gfs.createReadStream(file);
-      console.log('[+] User is downloading: ' + file.filename)
-      console.log('[+] URL Encoded filename: ' + encodeURIComponent(file.filename))
       // Set the File Type and Filename before sending the stream
       // res.writeHead(200, {
       //   "Content-Type": file.contentType,
@@ -222,22 +153,15 @@ router.get('/getfile/:_id', (req, res) => {
 // Delete One File
 // =============================================
 router.post('/delete/:id', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
-  console.log('[+] Delete file where id=' + req.params.id)
-
-  // This has 2 parts
-  // Part 1 = Delete from the list of files
-  // Part 2 = Delete the actual file from the DB using GridFS.remove
-
-
-  // Delete from the list of files
+  // Delete from "customerfiles" collection, then delete from GridFS
+  // Part 1: Delete from "customerfiles"
   customerfile
     .remove({fileID: req.params.id})
-    .then(() => {console.log("Deleted file: " + req.params.id )})
+    .then(() => {console.log(getTimeStamp() + req.user.sAMAccountName + " Deleted file: " + req.params.id )})
     .catch((error) => {return res.render("error.ejs", {message: error, user: req.user})})
 
-  // Delete from Grid FS
-
-  console.log('[+] Delete from GridFS: ' + req.params.id)
+  // Part 2: Delete from Grid FS
+  console.log(getTimeStamp() + req.user.sAMAccountName + ' Deleted from GridFS: ' + req.params.id)
   gfs.remove({_id: req.params.id, root: 'uploads'}, (err, gridStore) => {
     if(err) {
       return res.status(404).json({ err: err})
@@ -245,12 +169,21 @@ router.post('/delete/:id', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
       req.flash('message', 'File Deleted Successfully.')
       return res.redirect('back')
   })
-
 })
   
 
-
-
+// =============================================
+// Get Timestamp
+// =============================================
+function getTimeStamp() {
+  const date = new Date();
+  return '['+ date.getFullYear() + '-' +
+      (date.getMonth() + 1).toString().padStart(2, '0') + '-' +
+      date.getDate().toString().padStart(2, '0') + ':' +
+      date.getHours().toString().padStart(2, '0') + ':' +
+      date.getMinutes().toString().padStart(2, '0') + ':' +
+      date.getSeconds().toString().padStart(2, '0') + '] ';
+}
 
 
 // =============================================
